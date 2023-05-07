c1fd2585c2a2dd021bee040367d4f1b9ec2d62cc
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2023-05-07 12:59:31 +0200

    Eindopdracht - CharacterCatalog (Added dynamical generation for character profile)

--- character.php
+++ character.php
@@ -1,43 +1,55 @@
+<?php
+  include('connect.php');
+  $curChar = $_GET['character'];
+  if ($curChar) {
+    $pdo = DBconnect('localhost', 'rubyquest', 'root', 'mysql');
+    $charNames = queryHandling($pdo, "SELECT `name` FROM `characters`", 0)->fetchAll(PDO::FETCH_COLUMN);
+    if (in_array($curChar, array_values($charNames))) {
+      $charData = queryHandling($pdo, "SELECT * FROM `characters` WHERE `name`='" . $curChar . "'", 0)->fetch(PDO::FETCH_ASSOC);
+    } else {
+      echo 'INVALID CHARACTER!!!';
+      die();
+    }
+    terminatePDO($pdo);
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Character - Bowser</title>
+  <title>Character - <?php echo $charData['name']?></title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link href="resources/css/style.css" rel="stylesheet"/>
 </head>
 <body>
-<header><h1>Bowser</h1>
-  <a class="backbutton" href="index.html"><i class="fas fa-long-arrow-alt-left"></i> Terug</a></header>
+<header>
+  <h1><?php echo $charData['name']?></h1>
+  <a class="backbutton" href="index.php"><i class="fas fa-long-arrow-alt-left"></i> Terug</a>
+</header>
 <div id="container">
   <div class="detail">
     <div class="left">
-      <img class="avatar" src="resources/images/bowser.jpg">
-      <div class="stats" style="background-color: yellowgreen">
+      <img class="avatar" src="resources/images/<?php echo $charData['avatar']?>">
+      <div class="stats" style="background-color: <?php echo $charData['color']?>">
         <ul class="fa-ul">
-          <li><span class="fa-li"><i class="fas fa-heart"></i></span> 10000</li>
-          <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> 400</li>
-          <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> 100</li>
+          <li><span class="fa-li"><i class="fas fa-heart"></i></span> <?php echo $charData['health']?></li>
+          <li><span class="fa-li"><i class="fas fa-fist-raised"></i></span> <?php echo $charData['attack']?></li>
+          <li><span class="fa-li"><i class="fas fa-shield-alt"></i></span> <?php echo $charData['defense']?></li>
         </ul>
         <ul class="gear">
-          <li><b>Weapon</b>: Fire Breath</li>
-          <li><b>Armor</b>: Giant Shell</li>
+          <?php
+            if ($charData['weapon']) {
+              echo "<li><b>Weapon</b>: " . $charData['weapon'] . "</li>";
+            }
+            if ($charData['armor']) {
+              echo "<li><b>Armor</b>: " . $charData['armor'] . "</li>";
+            }     
+          ?>
         </ul>
       </div>
     </div>
     <div class="right">
-      <p>
-        Bowser or King Koopa, is a fictional character and the main antagonist of Nintendo's Mario franchise. In
-        Japan, the character bears the title of Great Demon King. In the U.S., the character was first referred
-        to as "Bowser, King of the Koopas" and "the sorcerer king" in the instruction manual.<br/>
-        <br/>
-        Bowser is the leader of the turtle-like Koopa race, and has been the archenemy of Mario since his first
-        appearance, in the 1985 video game Super Mario Bros.<br/>
-        <br/>
-        His ultimate goals are to kidnap Princess Peach, defeat Mario, and conquer the Mushroom Kingdom. Since
-        his debut, he has appeared in almost every Mario franchise game, usually serving as the main antagonist.
-        Bowser is voiced by Kenny James.
-      </p>
+      <p><?php echo $charData['bio']?></p>
     </div>
     <div style="clear: both"></div>
   </div>
